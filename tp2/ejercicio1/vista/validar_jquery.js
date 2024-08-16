bcb1ad54d5130fdcfb990075ee168bc72aecff0c
DiegoAvila1 <diegoavilaaa0@gmail.com>
2024-08-16 10:28:47 -0300

    arregle lo del equal

--- tp2/ejercicio1/vista/validar_jquery.js
+++ tp2/ejercicio1/vista/validar_jquery.js
@@ -1,41 +1,3 @@
-/*
-$.validator.setDefaults( {
-    submitHandler: function () {
-       alert( "submitted!" );
-    }
- });
-
-$(document).ready(function() {
-    $("#form").validate({
-        rules: {
-            name:{
-                required: true,
-                minlength: 3,
-            },
-            password:{
-                required: true,
-                minlength: 8,
-            }
-        }, messages: {
-            name: {
-                required: "Por favor, ingrese su nombre",
-                minlength: "El nombre debe tener al menos 3 caracteres",
-                notEqual: "El nombre no puede ser igual a la contraseña"
-            },
-            password: {
-                required: "Por favor, ingrese su contraseña",
-                minlength: "La contraseña debe tener al menos 8 caracteres"
-            }
-        }
-    })
-});*/
-
-
- // Añadir la regla de validación personalizada
- $.validator.addMethod('notEqual', function(value, element, param) {
-    return this.optional(element) || value !== $(param).val();
-}, 'La contraseña no puede ser igual al nombre.');
-
 // Establecer los valores por defecto para el manejador de envío
 $.validator.setDefaults({
     submitHandler: function() {
@@ -49,7 +11,7 @@ $(document).ready(function() {
             name: {
                 required: true,
                 minlength: 3,
-                notEqual: "#password" // Usa la regla personalizada
+                equalTo: "#password" // Usa la regla personalizada
             },
             password: {
                 required: true,
@@ -60,7 +22,6 @@ $(document).ready(function() {
             name: {
                 required: "Por favor, ingrese su nombre.",
                 minlength: "El nombre debe tener al menos 3 caracteres.",
-                notEqual: "La contraseña no puede ser igual al nombre." // Mensaje de error para la regla personalizada
             },
             password: {
                 required: "Por favor, ingrese su contraseña.",
